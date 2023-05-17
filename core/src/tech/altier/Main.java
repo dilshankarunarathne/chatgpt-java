22d9e209248bef4c354e6068feef56e35a4dfa57
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-05-17 18:25:14 +0530

    Update Main.java

--- core/src/tech/altier/Main.java
+++ core/src/tech/altier/Main.java
@@ -1,7 +1,43 @@
 package tech.altier;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+
+import org.json.JSONObject;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+    }
+
+    /**
+     * ChatGPT API caller function
+     * This method will take a string as input and return a string as output
+     * @param input
+     * @return response
+     */
+    public static String chat(String input) throws ProtocolException {
+        String URL = "https://api.openai.com/v1/completions";
+        HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Authorization", "Bearer YOUR-API-KEY");
+
+        JSONObject data = new JSONObject();
+        data.put("model", "text-davinci-003");
+        data.put("prompt", text);
+        data.put("max_tokens", 4000);
+        data.put("temperature", 1.0);
+
+        con.setDoOutput(true);
+        con.getOutputStream().write(data.toString().getBytes());
+
+        String output = new BufferedReader(new InputStreamReader(con.getInputStream())).lines()
+                .reduce((a, b) -> a + b).get();
+
+        System.out.println(new JSONObject(output).getJSONArray("choices").getJSONObject(0).getString("text"));
     }
 }
